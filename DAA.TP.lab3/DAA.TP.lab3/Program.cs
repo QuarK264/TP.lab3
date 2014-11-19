1365430964cd9c3220b965590f5ce26978858456
QuarK264 <alexey.danilov@hotmail.com>
2014-11-19 09:08:18 +0300

    2 курс, Технология программирования лабораторная 3

---
+++ DAA.TP.lab3/DAA.TP.lab3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DAA.TP.lab3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        private static double FuncBessel(int n, double x)
+        {
+            if (n == 0)
+            {
+                return x;
+            }
+            if (n == 1)
+            {
+                return 2 * x;
+            }
+            return 2 * (n - 1) * FuncBessel(n - 1, x) / x - FuncBessel(n - 2, x);
+        }
+        static void Main()
+        {
+            int n = 8;
+            var x = 2;
+            var RecurMethod = Stopwatch.StartNew();
+            Console.WriteLine(FuncBessel(n, x));
+            RecurMethod.Stop();
+            Console.WriteLine(RecurMethod.ElapsedTicks);
+            Console.ReadLine();
+        }
+    }
+}
