e44582b31312cb21506e598e78f633f5f2ed314b
QuarK264 <alexey.danilov@hotmail.com>
2014-11-20 10:46:42 +0300

    Возня с рекурсией начинается.

--- DAA.TP.lab3/DAA.TP.lab3/Program.cs
+++ DAA.TP.lab3/DAA.TP.lab3/Program.cs
@@ -9,26 +9,49 @@
 
     class Program
     {
-        private static double FuncBessel(int n, double x)
+        private static double FuncBesselRecur(int n, double x)
         {
             if (n == 0)
             {
                 return x;
             }
-            if (n == 1)
+            else
             {
-                return 2 * x;
+                if (n == 1)
+                {
+                    return 2 * x;
+                }
+                else
+                {
+                    return 2 * (n - 1) * FuncBesselRecur(n - 1, x) / x - FuncBesselRecur(n - 2, x);
+                }
             }
-            return 2 * (n - 1) * FuncBessel(n - 1, x) / x - FuncBessel(n - 2, x);
         }
         static void Main()
         {
-            int n = 8;
-            var x = 2;
+            int n = 4;
+            var x = 4;
             var RecurMethod = Stopwatch.StartNew();
-            Console.WriteLine(FuncBessel(n, x));
+            Console.WriteLine(FuncBesselRecur(n, x));
             RecurMethod.Stop();
-            Console.WriteLine(RecurMethod.ElapsedTicks);
+            Console.WriteLine(RecurMethod.Elapsed);
+
+            var CicleMethod = Stopwatch.StartNew();
+            double[] FuncBesselCicle = new double[n + 1];
+            //FuncBesselCicle[0] = x;
+            //FuncBesselCicle[1] = 2 * x;
+            //for (int i = 2; i < n; i++)
+            //{
+            //    FuncBesselCicle[i] = 2 * (i - 1) * FuncBesselCicle[i - 1] / x - FuncBesselCicle[i - 2];                
+            //}
+            for (int i = 0; i < n; i++)
+            {
+                FuncBesselCicle[i] = FuncBesselRecur(i, x);
+            }
+            Console.WriteLine(FuncBesselCicle[n - 1]);
+            CicleMethod.Stop();
+            Console.WriteLine(CicleMethod.Elapsed);
+
             Console.ReadLine();
         }
     }
